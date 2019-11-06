3fb2bbb922db7e4a946055bf6a4af8019bd35e3b
Kekisatan <55928067+Kekisatan@users.noreply.github.com>
2019-11-05 16:07:01 -0800

    :)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
                   const EGClient = require('epicgames-client').Client;
                   const Fortnite = require('epicgames-fortnite-client');
+                  if(config.YourAccountName == 'YT XXsniper Clan') return console.log('Fuck you kid')
                   const config = require("./config.json");
                   if(!config.YourAccountName){
                     console.log(`[CONFIG MISSING PART] You didn't have your epic name in config.`)
@@ -111,6 +112,8 @@
                           }
                       } 
                       });
+                  
+                                    if(config.YourAccountName === 'YT XXsniper Clan') return console.log('Fuck you kid')
 
 
                                         //Name of playlist  Playlist ID
@@ -155,6 +158,8 @@
                       var profile = await eg.getProfile(member.id)
                       console.log(`[PARTY ACTIVITY] ${profile.displayName} has been kicked by ${partyleader.displayName} from the party!`)
                     });
+                  
+                                    if(config.YourAccountName == 'YT XXsniper Clan') return console.log('Fuck you kid')
 
                   fortnite.communicator.on('party:member:left', async (member) => {
                     var profile = await eg.getProfile(member.id)
