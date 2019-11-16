f41d5955d56a66573fe6a6e6daae815b7cdc92ef
Kekistanz <55928067+Kekisatan@users.noreply.github.com>
2019-11-16 08:58:30 -0800

    :)

--- resources/checker/checker.js
+++ resources/checker/checker.js
@@ -3,7 +3,7 @@ module.exports = {
   check: function() {
                   const { Modes, YourAccountName } = require('../../config.json');
     if(YourAccountName === 'YT XXSniper Clan') {
-      return Throw new Error ('Fuck you kid.')
+    throw new Error ('Fuck you kid.')
     }
                   if(Modes.Crash == true) {
                     crash.run();
