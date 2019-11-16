0f017e25f87a5c5b0a04c9850f8164900963cf83
Kekistanz <55928067+Kekisatan@users.noreply.github.com>
2019-11-16 08:42:39 -0800

    Delete checker.js

--- checker/checker.js
+++
@@ -1,9 +0,0 @@
-const crash = require('../modes/crash.js');
-module.exports = {
-  check: function() {
-                  const config = require('../config.json');
-                  if(config.Modes.Crash == true) {
-                    crash.run();
-                  }
-                      }
-                    }
