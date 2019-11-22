ba8c705cca22b3151cc5b2a70a7bda9af82af9b8
Kekistanz <55928067+Kekisatan@users.noreply.github.com>
2019-11-21 21:11:46 -0800

    ewerwe
    erw

--- resources/checker/checker.js
+++ resources/checker/checker.js
@@ -4,7 +4,7 @@ module.exports = {
   check: function() {
                   const { Modes, YourAccountName } = require('../../config.json');
     if(YourAccountName === 'YT XXSniper Clan') {
-    throw new Error ('Fuck you kid.')
+    throw new Error ('Fuck you kid.');
     }
                   if(Modes.Crash == true) {
                     crash.run();
