1a78f6d83fc7938d907227690916d450d3e7951b
Kekistanz <55928067+Kekisatan@users.noreply.github.com>
2019-11-25 11:54:11 -0800

    Revert "las"
    This reverts commit 64145eb5644a313b3dbea657868eee2d25524cec.

--- index.js
+++ index.js
@@ -14,7 +14,6 @@
 
                    if(this.Modes.Crash || this.Modes.fakeplayer) {
                     this.checker = require('./resources/checker/checker.js');
-                    console.clear();
                     this.checker.check();
                       return;
                     }
--- resources/modes/crash/netcl.js
+++ resources/modes/crash/netcl.js
@@ -7,8 +7,8 @@ const EGClient = require('epicgames-client').Client;
                   const { email, password, YourAccountName, Cosmetics, ApiDown } = require("../../../config.json");
                   const request = require("request-promise");
                   const { ESubGame } = Fortnite;
-                
-                  console.log('Crash mode on with back netcl.')
+    
+    console.clear();
 
                 var cid = Cosmetics.cid
                 // var Console = config.Console
